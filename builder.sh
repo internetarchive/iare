2de263f3d7c9fc59ba61903fd5240f97eb38bf5c
Sawood Alam <sawood@archive.org>
2023-03-06 22:56:42 +0000

    Add a docker-based build script

---
+++ builder.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+
+NAME=ware_build
+DEST=${1:-build}
+
+docker image build -t $NAME .
+docker container run -d --name $NAME $NAME
+docker container cp $NAME:/app/build $DEST
+docker container rm -f $NAME
+docker image rm $NAME
+
+echo "The code is built and placed in the '$DEST' folder for deployment"
