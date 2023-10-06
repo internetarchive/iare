3270138e26c03ca3539b5c11e716b0f5110c07da
mojomonger <mojomonger@yahoo.com>
2023-10-05 23:54:56 -0700

    feature: add manage iabot link in Reference Viewer

--- src/components/v2/RefView/RefUrls.js
+++ src/components/v2/RefView/RefUrls.js
@@ -16,29 +16,43 @@ export default function RefUrls({ urls }) {
     const myIariBase = myConfig?.iariSource;
     const myStatusMethod = myConfig?.urlStatusMethod;
 
+//    https://iabot.wmcloud.org/index.php?page=manageurlsingle&url=http%3A%2F%2Findigenouspeoplesissues.com%2Findex.php%3Foption%3Dcom_content%26view%3Darticle%26id%3D14679%3Arapanui-protests-continue-against-the-hotel-hanga-roa%26catid%3D23%3Asouth-america-indigenous-peoples%26Itemid%3D56
+    const endpointIabot = 'https://iabot.wmcloud.org/index.php?page=manageurlsingle'
+
+    const headerRow = <tr>
+        <th className={'url-name'}><h3 className={"urls-header"}>Urls</h3></th>
+        <th className={'url-status'}>Status</th>
+        <th className={'url-iabot-status'}>IABot</th>
+    </tr>
+
+    const getDataRow = (u) => {
+        const classes = (u.status_code === 0 ? ' url-is-unknown'
+            : u.status_code >= 200 && u.status_code < 300 ? ' url-is-success'
+                : u.status_code >= 300 && u.status_code < 400 ? ' url-is-redirect'
+                    : u.status_code >= 400 && u.status_code < 500 ? ' url-is-notfound'
+                        : u.status_code >= 500 && u.status_code < 600 ? ' url-is-error'
+                            : '')
+
+        const endpoint = endpointIabot + `&url=${encodeURIComponent(u.url)}`
+        // const endpoint = endpointIabot + `&url=${encodeURI(u.url)}`
+
+        return <tr key={u.url} className={classes}>
+            <td><MakeLink href={u.url} /></td>
+            <td><div>{u.status_code}</div></td>
+            <td><div><MakeLink href={endpoint} linkText={"manage"} /></div></td>
+        </tr>
+    }
 
     // create display components (table, tr, and td) for array of url objects
     const urlsToRows = useCallback( (urlObjects=[]) => {
         return urlObjects
-            ? urlObjects.map( u => {
-                const classes = (u.status_code === 0 ? ' url-is-unknown'
-                        : u.status_code >= 200 && u.status_code < 300 ? ' url-is-success'
-                        : u.status_code >= 300 && u.status_code < 400 ? ' url-is-redirect'
-                        : u.status_code >= 400 && u.status_code < 500 ? ' url-is-notfound'
-                        : u.status_code >= 500 && u.status_code < 600 ? ' url-is-error'
-                        : '')
+            ? urlObjects.map( u => getDataRow(u) )
 
-                return <tr key={u.url} className={classes}>
-                    <td><MakeLink href={u.url} /></td>
-                    <td><div>{u.status_code}</div></td>
-                </tr>
-                }
-            )
-
-            // if urlObjects is undefined...TODO this NOT a good way to present errors to the user!
+            // if urlObjects is undefined...
+            // TODO this NOT a good way to present errors to the user!
             : <tr>
-            <td>URL data undefined</td>
-            <td>-</td>
+                <td>URL data undefined</td>
+                <td>-</td>
             </tr>
 
     }, [])
@@ -84,10 +98,7 @@ export default function RefUrls({ urls }) {
     const urlsDisplay =
         <table className={'ref-view-urls'}>
             <thead>
-            <tr>
-                <th className={'ref-view-url-name'}><h3 className={"urls-header"}>Urls</h3></th>
-                <th className={'ref-view-url-status'}>Status</th>
-            </tr>
+            {headerRow}
             </thead>
             <tbody>
             {urlRows}
