2714898414588806d8acff473d63462328c7889c
mojomonger <mojomonger@yahoo.com>
2025-05-19 02:30:39 -0700

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
-// import fs from 'fs/promises';
 
 export default defineConfig({
+
+    base: '/iare/',
+
     build: {
         outDir: 'dist',  /// changed from "/build"
         sourcemap: true, // Generates source maps for debugging;
